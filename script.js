44e391c45da2d39e883d14473619ad0b0ee2f26b
Bindloss <sam.bindloss@hotmail.com>
2018-03-13 12:32:02 +0000

    cleaned up portfolio page and blog page as well as implementing svgs in the footer.

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+// When the user scrolls down 70px from the top of the document, show the button
+window.onscroll = scrollFunction;
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+let intervalId = 0; // Needed to cancel the scrolling when we're at the top of the page
+const $scrollButton = document.querySelector('.scroll'); // Reference to our scroll button
+
+function scrollStep() {
+    // Check if we're at the top already. If so, stop scrolling by clearing the interval
+    if (window.pageYOffset === 0) {
+        clearInterval(intervalId);
+    }
+    window.scroll(0, window.pageYOffset - 50);
+}
+
+function scrollToTop() {
+    // Call the function scrollStep() every 16.66 millisecons
+    intervalId = setInterval(scrollStep, 12.66);
+}
+
+// When the DOM is loaded, this click handler is added to our scroll button
+$scrollButton.addEventListener('click', scrollToTop);
+
 var myImage = document.getElementById("mainImage");
 var imageArray = ["images/instawatch-portfolio.jpg", "images/richard-brown.jpg", "images/business-school.jpg"];			 
 var imageIndex = 0;
